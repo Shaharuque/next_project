1c728908789310318f99a9875291c50248e40263
devshaik444 <shahrukhamin27@gmail.com>
2022-12-30 01:11:46 -0800

    update broker list

--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Navbar from "../components/navbar/Navbar";
 
 const Contact = () => {
   return (
     <div>
-      <Navbar></Navbar>
       <h1>This is a contact page</h1>
     </div>
   );
