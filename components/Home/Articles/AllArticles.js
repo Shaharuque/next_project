7fa14e7458f393c254492d86edc356a2adccdc03
devshaik444 <shahrukhamin27@gmail.com>
2023-01-14 13:54:32 -0800

    updated

--- components/Home/Articles/AllArticles.js
+++ components/Home/Articles/AllArticles.js
@@ -7,28 +7,26 @@ const AllArticles = () => {
   const [data, setData] = useState(null);
   const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    fetch("http://67.205.165.138/api/article/list/")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data);
-        setLoading(false);
-      });
-  }, []);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   fetch("http://67.205.165.138/api/article/list/")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setData(data);
+  //       setLoading(false);
+  //     });
+  // }, []);
 
   if (isLoading) return <p className="text-center">Loading...</p>;
   if (!data) return <p>No profile data</p>;
   console.log(data);
   return (
     <div style={{ margin: "102px" }}>
-      <h2 style={{ textAlign: "end", marginBottom: "15px" }}>
-        {data?.success[1]?.description}
-      </h2>
-      <div
+      <h2 style={{ textAlign: "end", marginBottom: "15px" }}>Articles</h2>
+      {/* <div
         className="text-end mb-2"
         dangerouslySetInnerHTML={{ __html: data?.success[1]?.description }}
-      ></div>
+      ></div> */}
       <div className={styles.article_div}>
         {cards?.map((card, index) => (
           <Article key={index} card={card}></Article>
